7c7c7338be5c71d66a7c74e4623e17f8f1e4e2e9
arosspope <andrew.pope456@gmail.com>
2017-07-12 16:02:57 +1000

    initial wip on polybius square cipher

--- src/common/keygen.rs
+++ src/common/keygen.rs
@@ -1,5 +1,6 @@
 //! This module contains functions for the generation of keys.
 //!
+use std::collections::HashMap;
 use std::ascii::AsciiExt;
 use super::alphabet;
 
@@ -34,10 +35,54 @@ pub fn keyed_alphabet(key: &str, is_uppercase: bool) -> Result<String, &'static 
     Ok(keyed_alphabet)
 }
 
+pub fn polybius_square(key_square: &str, col_key: [char; 5], row_key: [char; 5])
+    -> Result<HashMap<String, char>, &'static str> {
+
+    if key_square.len() != 25 {
+        return Err("A polybius key square must have a length of 25 (alphabet key with i/j
+            combined).");
+    } else if !alphabet::is_alphabetic_only(key_square) {
+        return Err("A polybius key square cannot contain non-alphabetic symbols.");
+    }
+
+    //TODO: check that each element of the alphabet is present
+
+    //Check that i and j have been combined
+    if key_square.chars().into_iter().any(|c| c.eq_ignore_ascii_case(&'i')) &&
+        key_square.chars().into_iter().any(|c| c.eq_ignore_ascii_case(&'j'))
+    {
+        return Err("The characters 'i' & 'j' are combined in the polybius square. Only one of the
+            two can be included in the key squre.");
+    }
+
+    let mut square_chars = key_square.chars().into_iter();
+    let mut polybius_square = HashMap::new();
+
+    for i in 0..5 { //columns
+        for j in 0..5 { //rows
+            let mut key = String::from(row_key[j].to_string());
+            key.push(col_key[i]);
+            polybius_square.insert(key, square_chars.next().expect("Key square not big enough."));
+
+            //TODO: check if i or j then push a new entry if so
+        }
+    }
+
+    Ok(polybius_square)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn polybius_hashmap_order(){
+        let p = polybius_square("abcdefghiklmnopqrstuvwxyz", ['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e']).unwrap();
+
+        assert_eq!(&'a', p.get("aa").unwrap());
+        assert_eq!(&'c', p.get("ac").unwrap());
+        assert_eq!(&'e', p.get("ae").unwrap());
+    }
 
     #[test]
     fn generate_alphabet() {
---
+++ src/polybius.rs
@@ -0,0 +1,149 @@
+//! The Affine cipher is a special case of the more general monoalphabetic substitution cipher.
+//!
+//! The cipher is less secure than a substitution cipher as it is vulnerable to all of the attacks
+//! that work against substitution ciphers, in addition to other attacks. The cipher's primary
+//! weakness comes from the fact that if the cryptanalyst can discover the plaintext of two
+//! ciphertext characters, then the key can be obtained by solving a simultaneous equation
+//!
+use num::integer::gcd;
+use common::{substitute, alphabet, keygen};
+use common::cipher::Cipher;
+
+/// An Affine cipher.
+///
+/// This struct is created by the `new()` method. See its documentation for more.
+pub struct Polybius {
+    a_b: (usize, usize),
+}
+
+impl Cipher for Polybius {
+    type Key = (usize, usize);
+    type Algorithm = Polybius;
+
+
+    /// Initialise an Affine cipher given the keys `a` and `b`.
+    ///
+    /// Will return `Err` if one of the following conditions is detected:
+    ///
+    /// * `a` or `b` are not in the inclusive range `1 - 26`.
+    /// * `a` has a factor in common with 26.
+    fn new(a_b: (usize, usize)) -> Result<Polybius, &'static str> {
+        let p = keygen::polybius_square("wubalubadub dub", ['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e']);
+
+
+
+        Ok(Polybius {a_b: a_b})
+    }
+
+    /// Encrypt a message using an Affine cipher.
+    ///
+    /// # Examples
+    /// Basic usage:
+    ///
+    /// ```
+    /// use cipher_crypt::{Cipher, Affine};
+    ///
+    /// let a = Affine::new((3, 7)).unwrap();
+    /// assert_eq!("Hmmhnl hm qhvu!", a.encrypt("Attack at dawn!").unwrap());
+    /// ```
+    fn encrypt(&self, message: &str) -> Result<String, &'static str> {
+        // Encryption of a letter:
+        //         E(x) = (ax + b) mod 26
+        // Where;  x    = position of letter in alphabet
+        //         a, b = the numbers of the affine key
+
+        substitute::shift_substitution(message,
+            |idx| alphabet::modulo(((self.a_b.0*idx) + self.a_b.1) as isize))
+    }
+
+    /// Decrypt a message using an Affine cipher.
+    ///
+    /// # Examples
+    /// Basic usage:
+    ///
+    /// ```
+    /// use cipher_crypt::{Cipher, Affine};
+    ///
+    /// let a = Affine::new((3, 7)).unwrap();
+    /// assert_eq!("Attack at dawn!", a.decrypt("Hmmhnl hm qhvu!").unwrap());
+    /// ```
+    fn decrypt(&self, ciphertext: &str) -> Result<String, &'static str> {
+        // Decryption of a letter:
+        //         D(x) = (a^-1*(x - b)) mod 26
+        // Where;  x    = position of letter in alphabet
+        //         a^-1 = multiplicative inverse of the key number `a`
+        //         b    = a number of the affine key
+        let a_inv = alphabet::multiplicative_inverse(self.a_b.0 as isize)
+            .expect("Multiplicative inverse for 'a' could not be calculated.");
+
+        substitute::shift_substitution(ciphertext,
+            |idx| alphabet::modulo(a_inv as isize * (idx as isize - self.a_b.1 as isize)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn encrypt_message() {
+    //     let a = Affine::new((3, 7)).unwrap();
+    //     assert_eq!("Hmmhnl hm qhvu!", a.encrypt("Attack at dawn!").unwrap());
+    // }
+    //
+    // #[test]
+    // fn decrypt_message() {
+    //     let a = Affine::new((3, 7)).unwrap();
+    //     assert_eq!("Attack at dawn!", a.decrypt("Hmmhnl hm qhvu!").unwrap());
+    // }
+    //
+    // #[test]
+    // fn with_utf8(){
+    //     let a = Affine::new((15, 10)).unwrap();
+    //     let message = "Peace ✌️ Freedom and Liberty!";
+    //
+    //     assert_eq!(message, a.decrypt(&a.encrypt(message).unwrap()).unwrap());
+    // }
+    //
+    // #[test]
+    // fn exhaustive_encrypt(){
+    //     //Test with every combination of a and b
+    //     let message = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //
+    //     for a in 1..27 {
+    //         if gcd(a, 26) > 1 {
+    //             continue;
+    //         }
+    //
+    //         for b in 1..27 {
+    //             let a = Affine::new((a, b)).unwrap();
+    //             assert_eq!(message, a.decrypt(&a.encrypt(message).unwrap()).unwrap());
+    //         }
+    //     }
+    // }
+    //
+    // #[test]
+    // fn valid_key(){
+    //     assert!(Affine::new((15, 17)).is_ok());
+    // }
+    //
+    // #[test]
+    // fn b_shares_factor(){
+    //     assert!(Affine::new((15, 2)).is_ok());
+    // }
+    //
+    // #[test]
+    // fn a_shares_factor(){
+    //     assert!(Affine::new((2, 15)).is_err());
+    // }
+    //
+    // #[test]
+    // fn keys_to_small(){
+    //     assert!(Affine::new((0, 10)).is_err());
+    // }
+    //
+    // #[test]
+    // fn keys_to_big(){
+    //     assert!(Affine::new((30, 51)).is_err());
+    // }
+}
