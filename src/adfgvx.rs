c57d24cffec5873512a9ddf03a5fe9bcf9d491e0
arosspope <andrew.pope456@gmail.com>
2017-07-20 13:12:58 +1000

    wip on adfgvx cipher

--- src/adfgvx.rs
+++ src/adfgvx.rs
@@ -72,36 +72,13 @@ impl Cipher for ADFGVX {
     ///    p.encrypt("Attack 🗡️ the east wall").unwrap());
     /// ```
     fn encrypt(&self, message: &str) -> Result<String, &'static str> {
+        let mut columns = self.generate_columns();
+
         let polybius_ctext = substitute::polybius_encrypt(&self.square, message);
+        ADFGVX::transpose_to_columns(&mut columns, &polybius_ctext);
+        columns.sort_by(|a, b| a.0.cmp(&b.0));
 
-        let mut i = 0;
-        let mut columns: Vec<(char, Vec<Option<char>>)> = self.transposition_key.chars()
-                .into_iter()
-                .map(|c| (c, Vec::new()))
-                .collect();
-
-        for c in polybius_ctext.chars() {
-            columns[i].1.push(Some(c));
-            i = (i + 1) % columns.len();
-        }
-
-        let mut max_len = None;
-        if let Some(longest_v) = columns.iter().max_by_key(|v| v.1.len()) {
-            max_len = Some(longest_v.1.len());
-        }
-
-        if let Some(max_len) = max_len {
-            for &mut (_, ref mut v) in columns.iter_mut(){
-                let v_len = v.len();
-                v.extend(vec![None; (max_len - v_len)]);
-            }
-        }
-
-
-        println!("{:?}", columns);
-
-
-        Err("")
+        Ok(ADFGVX::transpose_to_text(&mut columns)) //TODO: THIS STEP IS WRONG
     }
 
     /// Decrypt a message using a Polybius square cipher.
@@ -119,32 +96,123 @@ impl Cipher for ADFGVX {
     ///    p.decrypt("BCdfdfbcbdgf 🗡️ dfgcbf bfbcbzdf ezbcacac").unwrap());
     /// ```
     fn decrypt(&self, ciphertext: &str) -> Result<String, &'static str> {
-        //We read the ciphertext two bytes at a time and transpose the original message using the
-        //polybius square
         let mut message = String::new();
-        let mut buffer = String::new();
 
-        for c in ciphertext.chars().into_iter() {
-            //Determine if the character could potentially be part of a 'polybius sequence' to
-            //be decrypted. Only standard alphabetic characters can be part of a valid sequence.
-            match alphabet::STANDARD.find_position(c) {
-                Some(_) => buffer.push(c),
-                None => message.push(c)
+        //Construct the columns and sort by alphabetical order
+        let mut columns = self.generate_columns();
+        columns.sort_by(|a, b| a.0.cmp(&b.0));
+
+        ADFGVX::transpose_to_columns(&mut columns, ciphertext);
+
+
+        //Re-order the columns so that it is back in the order of the transposition key
+        self.reorder_to_transposition_key(&mut columns);
+
+        let polybius_sequence = ADFGVX::transpose_to_text(&mut columns);
+        println!("{}", polybius_sequence);
+
+        let m = substitute::polybius_decrypt(&self.square, &polybius_sequence).unwrap();
+
+        println!("{}", m);
+        Ok(m)
+    }
+}
+
+impl ADFGVX {
+    fn reorder_to_transposition_key(&self, columns: &mut Vec<(char, Vec<Option<char>>)>)
+        //-> Result<_, &'static str>
+    {
+        for (index, column_id) in self.transposition_key.chars().enumerate() {
+            if let Some(pos) = columns.iter().position(|v| v.0 == column_id) {
+                let column_to_move = columns[pos].clone();
+                columns.remove(pos);
+                columns.insert(index, column_to_move);
             }
 
-            if buffer.len() == 2 {
-                match self.square.get(&buffer) {
-                    Some(&val) => message.push(val),
-                    None => return Err("Unknown sequence in the ciphertext."),
-                }
+            // match columns.iter().position(|v| v.0 == column_id) {
+            //     Some(pos) => {
+            //         let column_to_move = columns[pos].clone();
+            //         columns.remove(pos);
+            //         columns.insert(index, column_to_move);
+            //     },
+            //     //None => _
+            // }
+        }
+    }
 
-                buffer.clear();
+    fn generate_columns(&self) -> Vec<(char, Vec<Option<char>>)> {
+        self.transposition_key.chars()
+                .into_iter()
+                .map(|c| (c, Vec::new()))
+                .collect()
+    }
+
+    fn transpose_to_columns(columns: &mut Vec<(char, Vec<Option<char>>)>, text: &str) {
+        let mut i = 0;
+        for c in text.chars() {
+            columns[i].1.push(Some(c));
+            i = (i + 1) % columns.len();
+        }
+
+        ADFGVX::expand_columns(columns);
+    }
+
+    fn expand_columns(columns: &mut Vec<(char, Vec<Option<char>>)>) {
+        //Expand each column so that it is of a fixed length
+        let mut length = None;
+        if let Some(longest_v) = columns.iter().max_by_key(|v| v.1.len()) {
+            length = Some(longest_v.1.len());
+        }
+
+        if let Some(max) = length {
+            for &mut (_, ref mut v) in columns.iter_mut(){
+                let v_len = v.len();
+                v.extend(vec![None; (max - v_len)]);
+            }
+        }
+    }
+
+    fn transpose_to_text2(columns: Vec<(char, Vec<Option<char>>)>) -> String {
+        let mut text = String::new();
+        let mut i = 0;
+
+        for column in columns.iter() {
+            for character in column.1.iter() {
+                if let Some(c) = character {
+
+                }
             }
         }
 
-        Ok(message)
+        text
+    }
+
+    fn transpose_to_text(columns: &mut Vec<(char, Vec<Option<char>>)>) -> String {
+        let mut text = String::new();
+        let mut i = 0;
+
+
+        //While at least one of the columns have not been completely emptied
+        while columns.iter().any(|column| column.1.len() > 0) {
+            if columns[i].1.len() < 1 {
+                i = (i + 1) % columns.len();
+                continue; //Skip if already empty
+            }
+
+            //If their is a valid char to consume, push it to the transposed text
+            if let Some(c) = columns[i].1[0] {
+                text.push(c);
+            }
+
+            //This character in the column has been consumed, remove, and shuffle to the next.
+            columns[i].1.remove(0);
+            i = (i + 1) % columns.len();
+        }
+
+        text
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -152,9 +220,18 @@ mod tests {
 
     #[test]
     fn encrypt_message() {
-        let a = ADFGVX::new((String::from("secret"), String::from("cargo"))).unwrap();
+        let a = ADFGVX::new((String::from("53empb8d4yl9j1w0ci6asogq2kuthzv7fxnr"),
+            String::from("cargo"))).unwrap();
 
-        assert!(a.encrypt("attack dawn!").is_ok());
+        assert_eq!("DGDDFGGVGFGFDGVVVVDVDGDX", a.encrypt("ATTACKATDAWN").unwrap());
+    }
+
+    #[test]
+    fn decrypt_message() {
+        let a = ADFGVX::new((String::from("53empb8d4yl9j1w0ci6asogq2kuthzv7fxnr"),
+            String::from("cargo"))).unwrap();
+
+        assert_eq!("ATTACKATDAWN", a.decrypt("DGGVVGGFVDDVDVGDGGDDFFVX").unwrap());
     }
 
     // #[test]
