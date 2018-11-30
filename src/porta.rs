b4ba6b29808ae7d980cad6046e57dd7f3bcc2f08
arosspope <andrew.pope456@gmail.com>
2018-11-30 16:46:34 +1100

    clippy passes on all toolchains

--- src/porta.rs
+++ src/porta.rs
@@ -33,7 +33,7 @@ use common::cipher::Cipher;
 use common::keygen::cyclic_keystream;
 use common::substitute;
 
-#[cfg_attr(rustfmt, rustfmt_skip)]
+#[rustfmt::skip]
 const SUBSTITUTION_TABLE: [[usize; 26]; 13] = [
     [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12],
     [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 13, 12,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11],
